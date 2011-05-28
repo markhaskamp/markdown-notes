ea11d3c83688ee32f85d9d12fc7faf3f23b40f90
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-05-28 19:45:17 -0400

    changed color scheme. it's quite bad

--- data/html/scripts/md2html.js
+++ data/html/scripts/md2html.js
@@ -6,12 +6,17 @@ $(document).ready( function() {
 
 
   $('.collapse').each( function(ndx, ele) {
+    var title = $(ele).attr('title');
+    if (title === undefined) {
+      title = ''
+    }
     var hold_html = $(ele).html();
-    hold_html = '<span class="collapse_toggle">+-</span>\n<div class="collapse_region">' + hold_html;
+    hold_html = '<span class="collapse_toggle">+/- ' + title + '</span>\n<div class="collapse_region">' + hold_html;
     hold_html += '\n<div>';
     $(ele).html(hold_html);
 
   });
+  $('.collapse_region').toggle();
   $('.collapse_toggle').click( function() { $(this).next().toggle(); });
 
 $('.collapse_toggle').hover( function() { $(this).css('cursor', 'pointer'); },
