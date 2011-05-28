08164cf468bdd29bf424e55f728c10998c061cf3
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-05-28 18:48:32 -0400

    moved javascript to md2html.js

--- data/html/scripts/md2html.js
+++ data/html/scripts/md2html.js
@@ -3,5 +3,16 @@ $(document).ready( function() {
                  minlen: 0
                 ,headers: {use: true, depth: 4} 
               });
+
+
+  var hold_html = $('.collapse').html();
+  //  console.log(hold_html);
+  hold_html = '<span class="collapse_toggle">+-</span>\n<div class="collapse_region">' + hold_html
+  hold_html += '\n<div>'
+  $('.collapse').html(hold_html);
+
+  $('.collapse_region').toggle();
+  $('.collapse_toggle').click( function() { $('.collapse_region').toggle(); });
+
 });
 
