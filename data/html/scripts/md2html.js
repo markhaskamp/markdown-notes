8704e086aa014e36f194f799cc4bc514cffa90f9
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-05-28 19:08:09 -0400

    can show/hide multiple regions

--- data/html/scripts/md2html.js
+++ data/html/scripts/md2html.js
@@ -5,12 +5,13 @@ $(document).ready( function() {
               });
 
 
-  var hold_html = $('.collapse').html();
-  //  console.log(hold_html);
-  hold_html = '<span class="collapse_toggle">+-</span>\n<div class="collapse_region">' + hold_html
-  hold_html += '\n<div>'
-  $('.collapse').html(hold_html);
+  $('.collapse').each( function(ndx, ele) {
+    var hold_html = $(ele).html();
+    hold_html = '<span class="collapse_toggle">+-</span>\n<div class="collapse_region">' + hold_html;
+    hold_html += '\n<div>';
+    $(ele).html(hold_html);
 
+  });
   $('.collapse_toggle').click( function() { $(this).next().toggle(); });
 
 });
